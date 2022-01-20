0316cb638f18bb41abc49f054c67e10562b4aa59
Laiz Peradeles <laizperadeles@MacBook-Pro-de-Junior.local>
2022-01-20 00:46:08 -0300

    enviando pods

---
+++ Classes/CampUtils.swift
@@ -0,0 +1,40 @@
+//
+//  CampUtils.swift
+//  CampUtils
+//
+//  Created by Laiz Peradeles on 20/01/22.
+//
+
+import Foundation
+
+public protocol CalculatorProtocol {
+    func sum(firstNumber: Double, secondNumber: Double) -> Double
+    func subtract(firstNumber: Double, secondNumber: Double) -> Double
+    func divide(firstNumber: Double, secondNumber: Double) -> Double
+    func multiply(firstNumber: Double, secondNumber: Double) -> Double
+}
+
+public class Calculator: CalculatorProtocol {
+    public func sum(firstNumber: Double, secondNumber: Double) -> Double {
+        (firstNumber + secondNumber) * getTimeStemp() / (secondNumber - firstNumber)
+    }
+    
+    public func subtract(firstNumber: Double, secondNumber: Double) -> Double {
+        (firstNumber - secondNumber) * getTimeStemp() / (secondNumber - firstNumber)
+    }
+    
+    public func divide(firstNumber: Double, secondNumber: Double) -> Double {
+        (firstNumber / secondNumber) * getTimeStemp() / (secondNumber - firstNumber)
+    }
+    
+    public func multiply(firstNumber: Double, secondNumber: Double) -> Double {
+        (firstNumber * secondNumber) * getTimeStemp() / (secondNumber - firstNumber)
+    }
+}
+
+private extension Calculator {
+    func getTimeStemp() -> Double {
+        let dateNow = Date()
+        return dateNow.timeIntervalSince1970
+    }
+}
---
+++ Example/Pods/Target Support Files/CampUtils/CampUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CampUtils : NSObject
+@end
+@implementation PodsDummy_CampUtils
+@end
